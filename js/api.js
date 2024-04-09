c5ea3fab7261fe5f14aed61a157c834deaa6723f
Chelsea <cjohnson594@outlook.com>
2024-04-08 21:38:35 -0700

    third push

--- js/api.js
+++ js/api.js
@@ -1,10 +1,22 @@
 function getNearestBrew() {
+  const resultList = document.getElementById("result");
+  resultList.innerHTML = "";
   const getBreweriesNearCoordinates = (position) => {
     fetch(
-      `https://api.openbrewerydb.org/v1/breweries?by_dist=${position.coords.latitude},${position.coords.longitude}&per_page=3`
-    ).then((res) => {
-      console.log(res);
-    });
+      `https://api.openbrewerydb.org/v1/breweries?by_dist=${position.coords.latitude},${position.coords.longitude}&per_page=5`
+    )
+      .then((response) => response.json())
+      .then((res) => {
+        console.log(res);
+        for (let i = 0; i < res.length; i++) {
+          const result = res[i];
+
+          const resultItem = document.createElement("li");
+          resultItem.textContent = result.name;
+
+          resultList.appendChild(resultItem);
+        }
+      });
   };
   navigator.geolocation.getCurrentPosition(getBreweriesNearCoordinates);
 }

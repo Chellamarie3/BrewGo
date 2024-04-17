4913840c9d7baa42dea2b3250a81b2d5d5875ae0
Chelsea <cjohnson594@outlook.com>
2024-04-16 21:09:45 -0700

    combined all files and added an about us page

--- js/api.js
+++ js/api.js
@@ -8,11 +8,45 @@ function getNearestBrew() {
       .then((response) => response.json())
       .then((res) => {
         console.log(res);
+
         for (let i = 0; i < res.length; i++) {
           const result = res[i];
 
-          const resultItem = document.createElement("li");
+          const resultItem = document.createElement("div");
           resultItem.textContent = result.name;
+          resultItem.classList.add("card");
+          resultItem.style.width = "300px";
+
+          const img = document.createElement("img");
+          img.classList.add("card-img-top");
+          img.src = "img/Beer-Logo.jpg";
+          img.alt = "Brewery picture";
+
+          const cardBodyDiv = document.createElement("div");
+          cardBodyDiv.classList.add("card-body");
+
+          const cardTitle = document.createElement("h5");
+          cardTitle.classList.add("card-title");
+          cardTitle.textContent = result.address_1;
+
+          const cardText = document.createElement("a");
+          cardText.classList.add("card-text");
+          cardText.textContent = result.phone;
+          cardText.href = `tel:${result.phone}`;
+          cardText.classList.add("btn", "btn-warning", "me-4");
+
+          const link = document.createElement("a");
+          link.classList.add("btn", "btn-warning");
+          link.href = result.website_url;
+          link.target = "_blank";
+          link.textContent = "Visit Website";
+
+          cardBodyDiv.appendChild(cardTitle);
+          cardBodyDiv.appendChild(cardText);
+          cardBodyDiv.appendChild(link);
+
+          resultItem.appendChild(img);
+          resultItem.appendChild(cardBodyDiv);
 
           resultList.appendChild(resultItem);
         }
